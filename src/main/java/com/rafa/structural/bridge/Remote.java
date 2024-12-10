e3a8de96781f199cd86130491b4ec4f79ea94437
Rafael Roani Gonçalves <rafaelgoncalvesroani@gmail.com>
2024-12-09 21:15:42 -0300

    Structural - Bridge

---
+++ src/main/java/com/rafa/structural/bridge/Remote.java
@@ -0,0 +1,13 @@
+package com.rafa.structural.bridge;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
